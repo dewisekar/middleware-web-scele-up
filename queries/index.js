fae2f14b608a6c09502c6981782fd503a0296e9d
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-01-12 23:49:49 +0700

    move query to file

---
+++ queries/index.js
@@ -0,0 +1,27 @@
+const QUERIES = {
+GET_POST_DETAIL_QUERY : `SELECT 
+    a.[Tgl Post Sesuai Jadwal] as deadlineDate, 
+    a.[Tgl Post Real] as uploadDate,
+    a.[Post Id] as postId,
+    a.[Kontrak Id] as contractId,
+    a.[Link Post] as linkPost,
+    c.[Name] + ' (' + CONVERT(VARCHAR,d.[Kontrak Ke]) + ')' as contractName,
+    c.[Name] as kolName,
+    c.Platform as platform,
+    c.Username as username,
+    a.[Slot Ke] as  slotNumber,
+    e.[Tema] as briefName,
+    f.[Brief Code] as briefCode,
+    g.[Manager Name] as kolManager
+    FROM [MARKETING].dbo.Post a
+    JOIN [MARKETING].dbo.[Kol Kontrak] b WITH(NOLOCK) on a.[Kontrak Id] = b.[Kontrak Id]
+    JOIN [MARKETING].dbo.Kol c WITH(NOLOCK) on b.[Kol Id] = c.[Kol Id] 
+    JOIN [MARKETING].dbo.[Kol Kontrak Status] d WITH(NOLOCK) on d.[Kontrak Id] = a.[Kontrak Id] 
+    JOIN [MARKETING].dbo.Brief e WITH(NOLOCK) on a.[Brief Id] = e.[Brief Id] 
+    JOIN [MARKETING].dbo.[Brief Status] f WITH(NOLOCK) ON e.[Brief Id] = f.[Brief Id]
+    JOIN [MARKETING].dbo.[Kol Manager] g ON a.[Manager Id] = g.[Manager Id] 
+    WHERE a.[Post Id] = @postId`,
+GET_CONTRACT_DETAIL_QUERY: `select * from [MARKETING].[dbo].[Kol Kontrak] where [Kontrak Id] = @contractId`
+}
+
+module.exports =  {QUERIES}
--- routes/Marketing.js
+++ routes/Marketing.js
@@ -4,6 +4,8 @@ const { sendToTheQueue } = require("../utility/rabbitmq");
 const axios = require("axios");
 const { response } = require("express");
 
+const {QUERIES} = require("../queries/index")
+
 const pythonUrl = "http://144.126.214.16:3131";
 const sendEmail = async (receiverEmail, subject, content) => {
   let response = "failed";
@@ -220,9 +222,10 @@ const GetKontrakDetailByID = async (req) => {
   try {
     let id = req.Id;
     const pool = await poolPromise;
-    const query = `select * from [MARKETING].[dbo].[Kol Kontrak] where [Kontrak Id] = ${id}`
+    const query = QUERIES.GET_CONTRACT_DETAIL_QUERY
     const result = await pool
     .request()
+    .input("contractId", id)
     .query(query);
     console.log(result.recordset);
 
@@ -786,30 +789,10 @@ const getPostDetail = async (req) => {
   try {
     const {id} = req;
     const pool = await poolPromise;
-    const query = `SELECT 
-    a.[Tgl Post Sesuai Jadwal] as deadlineDate, 
-    a.[Tgl Post Real] as uploadDate,
-    a.[Post Id] as postId,
-    a.[Kontrak Id] as contractId,
-    a.[Link Post] as linkPost,
-    c.[Name] + ' (' + CONVERT(VARCHAR,d.[Kontrak Ke]) + ')' as contractName,
-    c.[Name] as kolName,
-    c.Platform as platform,
-    c.Username as username,
-    a.[Slot Ke] as  slotNumber,
-    e.[Tema] as briefName,
-    f.[Brief Code] as briefCode,
-    g.[Manager Name] as kolManager
-    FROM [MARKETING].dbo.Post a
-    JOIN [MARKETING].dbo.[Kol Kontrak] b WITH(NOLOCK) on a.[Kontrak Id] = b.[Kontrak Id]
-    JOIN [MARKETING].dbo.Kol c WITH(NOLOCK) on b.[Kol Id] = c.[Kol Id] 
-    JOIN [MARKETING].dbo.[Kol Kontrak Status] d WITH(NOLOCK) on d.[Kontrak Id] = a.[Kontrak Id] 
-    JOIN [MARKETING].dbo.Brief e WITH(NOLOCK) on a.[Brief Id] = e.[Brief Id] 
-    JOIN [MARKETING].dbo.[Brief Status] f WITH(NOLOCK) ON e.[Brief Id] = f.[Brief Id]
-    JOIN [MARKETING].dbo.[Kol Manager] g ON a.[Manager Id] = g.[Manager Id] 
-    WHERE a.[Post Id] = ${id}`
+    const query = QUERIES.GET_POST_DETAIL_QUERY
     const result = await pool
     .request()
+    .input("postId", id)
     .query(query);
     console.log(result.recordset);
 
