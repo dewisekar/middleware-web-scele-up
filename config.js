06298f7694450aa339c4a47d2179a31e699a5d5b
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-01-25 09:18:34 +0700

    put url on env

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// const PYTHON_URL = "http://144.126.214.16:3131";
-const PYTHON_URL = "http://127.0.0.1:5000/";
+const WA_URL = process.env.WA_URL
+const PYTHON_URL = process.env.PYTHON_URL
 
-module.exports = {PYTHON_URL}
+module.exports = { PYTHON_URL, WA_URL };
