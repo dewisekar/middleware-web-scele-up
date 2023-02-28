f3b7d22066b14f68e548485258a215b0f77d4307
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-02-28 19:33:27 +0700

    change cpm query

--- queries/index.js
+++ queries/index.js
@@ -240,13 +240,14 @@ where a.[Kontrak Id] = @contractId`,
     from MARKETING.dbo.Post_View a
     JOIN MARKETING.dbo.Post b on b.[Post Id] = a.postId 
     GROUP BY right('00' + CAST(MONTH(b.[Tgl Post Real]) AS VARCHAR(2)), 2) +'-'+ CAST(YEAR(b.[Tgl Post Real]) AS VARCHAR(4))
-    ) as c on c.yearMonth = yearMonth
+    ) as c on c.yearMonth = right('00' + CAST(MONTH(b.[Tgl Post Real]) AS VARCHAR(2)), 2) +'-'+ CAST(YEAR(b.[Tgl Post Real]) AS VARCHAR(4)) and a.views = c.views
     JOIN MARKETING.dbo.[Kol Kontrak] d on d.[Kontrak Id] = b.[Kontrak Id] 
-    JOIN MARKETING.dbo.Kol e on e.[Kol Id] = d.[Kol Id] 
-    where a.views = c.views`,
+    JOIN MARKETING.dbo.Kol e on e.[Kol Id] = d.[Kol Id] `,
   GET_MAX_CPM_PER_MONTH: `SELECT right('00' + CAST(MONTH(c.[Tgl Post Real]) AS VARCHAR(2)), 2) +'-'+ CAST(YEAR(c.[Tgl Post Real]) AS VARCHAR(4))  as yearMonth,
     (d.[Cost Per Slot]/a.views*1000) as cpm, f.Name as kolName, f.Platform as platform
     FROM MARKETING.dbo.Post_View a
+    JOIN MARKETING.dbo.Post c on a.postId = c.[Post Id] 
+    JOIN MARKETING.dbo.[Kol Kontrak Status] d on c.[Kontrak Id] = d.[Kontrak Id]
     JOIN
     (
     SELECT MAX(c.[Cost Per Slot]/a.views*1000) as maxCpm,
@@ -257,9 +258,7 @@ where a.[Kontrak Id] = @contractId`,
     where a.dayNumber = 7
     GROUP BY 
     right('00' + CAST(MONTH(b.[Tgl Post Real]) AS VARCHAR(2)), 2) +'-'+ CAST(YEAR(b.[Tgl Post Real]) AS VARCHAR(4))
-    ) as b on b.yearMonth = yearMonth
-    JOIN MARKETING.dbo.Post c on a.postId = c.[Post Id] 
-    JOIN MARKETING.dbo.[Kol Kontrak Status] d on c.[Kontrak Id] = d.[Kontrak Id] 
+    ) as b on b.yearMonth = right('00' + CAST(MONTH(c.[Tgl Post Real]) AS VARCHAR(2)), 2) +'-'+ CAST(YEAR(c.[Tgl Post Real]) AS VARCHAR(4)) 
     JOIN MARKETING.dbo.[Kol Kontrak] e on e.[Kontrak Id] = c.[Kontrak Id]
     JOIN MARKETING.dbo.Kol f on f.[Kol Id] = e.[Kol Id] 
     WHERE b.maxCpm = (d.[Cost Per Slot]/a.views*1000)`,
