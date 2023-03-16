0f30a6e52f325d47831441f703fbd4b98afa97ad
Dewi Sekarini <dewi.sekarini@btpn.com>
2023-03-17 00:51:18 +0700

    update activekol query

--- queries/index.js
+++ queries/index.js
@@ -269,7 +269,7 @@ where a.[Kontrak Id] = @contractId`,
     JOIN MARKETING.dbo.Kol f on f.[Kol Id] = e.[Kol Id] 
     WHERE b.maxCpm = (COALESCE(d.[Cost Per Slot]/NULLIF (a.views, 0)*1000, 0))`,
   GET_BANK_LIST: 'SELECT * FROM MARKETING.dbo.bank',
-  GET_ACTIVE_KOL: `select DISTINCT a.Name as kolName, a.[No Whatsapp] as phoneNumber, a.[Kol Id] as kolId, a.[Kategori Kol] as kolCategoryId, 
+  GET_ACTIVE_KOL: `select DISTINCT a.Name as kolName, a.[No Whatsapp] as phoneNumber, a.[Kol Id] as kolId, a.[Platform] as platform, a.[Kategori Kol] as kolCategoryId, 
   CAST(CASE WHEN b.[Kontrak Id] is NOT NULL AND DATEDIFF(day, getDate() , b.[Masa Kontrak Akhir] ) > 0  THEN 'YES' ELSE 'NO' END AS varchar) as isHasActiveContract
   from marketing.dbo.Kol a
   left Join MARKETING.dbo.[Kol Kontrak] b on a.[Kol Id] = b.[Kol Id]`,
